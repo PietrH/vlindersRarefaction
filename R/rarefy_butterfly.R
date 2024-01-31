e4c2d011d93de96a0c7878fe73dd183caf88391f
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2024-01-31 11:58:23 +0100

    add function skeleton

---
+++ R/rarefy_butterfly.R
@@ -0,0 +1,11 @@
+#' Calculate rarefaction curve of Butterfly data
+#'
+#' @param variables input variables (skeleton)
+#'
+#' @return iNext object of rarefied and extrapolated values
+#' @export
+#'
+#' @examples
+rarefy_butterfly <- function(variables) {
+
+}
