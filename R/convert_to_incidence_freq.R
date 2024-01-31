82a04a466051800dbeadfa56313d1ef890d1c50d
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2024-01-31 15:27:28 +0100

    add function to convert known format to incidence frequency

---
+++ R/convert_to_incidence_freq.R
@@ -0,0 +1,87 @@
+#' Convert a known input format to the incidence frequency format that iNEXT expects
+#'
+#' iNEXT expects incidence frequency data to be a list, with one child per
+#' assemblage. Every child of this list starts with an integer equal to the
+#' total number of sampling units (days, plots, ...), followed by one element
+#' per species equal to the number of sampling units this species was observed
+#' in.
+#'
+#' @param input_dataframe
+#'
+#' @return a list in the format as is expected by iNEXT when datatype is set to incidence_freq
+#' @export
+#'
+#' @examples \dontrun{convert_to_incidence_freq(warande)}
+convert_to_incidence_freq <- function(input_dataframe) {
+  # assert that the input has the expected shape
+
+  ## the right datatype
+  assertthat::assert_that(is.data.frame(input_dataframe))
+  ## the right columns
+  assertthat::assert_that(setequal(
+    colnames(input_dataframe),
+    c("date", "species_name", "year", "number", "MicroMacro")
+  ), msg = glue::glue(
+    "The columns in `input_dataframe` don't match the expected columns:",
+    "we expected: {expected_columns_sep}", " but found: {actual_columns_sep}",
+    expected_columns_sep =
+      glue::glue_collapse(
+        glue::backtick(
+          "date",
+          "species_name",
+          "year",
+          "number",
+          "MicroMacro"
+        ),
+        sep = ",", last = "&"
+      ),
+    actual_columns_sep =
+      glue::glue_collapse(
+        glue::backtick(colnames(input_dataframe)),
+        sep = ",", last = "&"
+      )
+  ))
+  ## MicroMacro can only have Macro or Micro as values
+  assertthat::assert_that(
+    setequal(unique(input_dataframe$MicroMacro), c("Macro", "Micro")),
+    msg = "the column `MicroMacro` can only have the values `Macro` or `Micro`"
+  )
+
+
+    # output should be a list, with the first element the number of sampling
+    # units (how often did you look? howmany plots/days), and then elements
+    # every element is a species en in howmany of the sampling units they were
+    # found
+
+
+  # group input dataframe ---------------------------------------------------
+
+  grouped_df <- input_dataframe %>% dplyr::group_by(MicroMacro)
+
+
+
+  # calculate number of sampling units --------------------------------------
+
+  number_of_sampling_units <-
+    grouped_df %>%
+      dplyr::group_map(~dplyr::n_distinct(dplyr::pull(.x,date))) %>%
+      purrr::set_names(c("Macro", "Micro"))
+
+
+  # calculate incidence frequencies -----------------------------------------
+
+  incidence_frequencies <-
+    grouped_df %>%
+      dplyr::group_map(~dplyr::count(.x, species_name)) %>%
+      purrr::map(~dplyr::pull(.x, n)) %>%
+      purrr::set_names(c("Macro", "Micro"))
+
+
+  # combine into output obj -------------------------------------------------
+
+  list(
+    Macro = c(number_of_sampling_units$Macro, incidence_frequencies$Macro),
+    Micro = c(number_of_sampling_units$Micro, incidence_frequencies$Micro)
+  )
+
+}
---
+++ tests/testthat/test-convert_to_incidence_freq.R
@@ -0,0 +1,31 @@
+warande <-
+  readr::read_delim("../../tblWarandepark.csv",
+                    delim = ";",
+                    show_col_types = FALSE)
+
+test_that("convert_to_incidence_freq() returns number of sampling units in first element", {
+  n_macro_sampling_units <-
+    warande %>%
+    dplyr::filter(MicroMacro == "Macro") %>%
+    dplyr::pull(date) %>%
+    dplyr::n_distinct()
+
+  expect_identical(
+    convert_to_incidence_freq(warande)$Macro[[1]],
+    n_macro_sampling_units
+  )
+})
+
+test_that("convert_to_incidence_freq() returns number of species + 1 as number of elements", {
+
+  number_of_macro_species <-
+    dplyr::filter(warande, MicroMacro == "Macro") %>%
+    dplyr::pull(species_name) %>%
+    dplyr::n_distinct() %>%
+    as.double() # to match output of convert_to_incidence_freq()
+
+  expect_identical(
+    length(convert_to_incidence_freq(warande)$Macro) - 1,
+    number_of_macro_species
+  )
+})
