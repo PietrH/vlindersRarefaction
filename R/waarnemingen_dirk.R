ead9758b609283addd126e5608356a84397bee6e
PietrH <pieterhuy@gmail.com>
2024-06-17 18:49:41 +0200

    add data object documentation

---
+++ R/waarnemingen_dirk.R
@@ -0,0 +1,8 @@
+#' Example waarnemingen.be export
+#'
+#' @format A dataframe with some exported waarnemingen.be observations
+#' @examples
+#' \dontrun{
+#'  waaarnemingen_dirk
+#' }
+"waarnemingen_dirk"
